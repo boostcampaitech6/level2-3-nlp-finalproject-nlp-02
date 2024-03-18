a649e3a4219f98cf0ba591cc0955b6a8bad2c615
C7C4FF <c7c4ff.beom@gmail.com>
2024-03-18 15:00:20 +0900

    feat: 이력 내역 페이지 생성
    [#10]

---
+++ pages/history.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# response = requests.get(url=me/result)
+# if response.status_code == 200:
+#     data = response.json()
+#     st.write(data)
+
+
+# TODO: 날짜로 받아오는 함수 만들기 - feedback 페이지 에서도 동일하게 작동되도록
+# response = requetes.get(url=me/result/{date})
+# if response.status_code == 200:
+    # data = response.json()
+    # st.write(data)
