da515126d50349f813b390d27b2a8207e25f21f2
9ooDa <dayeonkuhk@gmail.com>
2024-03-19 04:44:38 +0900

    [FEAT] Grammar GECToR 모델 추가 [#12]

---
+++ models/gector/test_main.py
@@ -0,0 +1,82 @@
+import os
+from gector import (
+    GECToR,
+    predict,
+    load_verb_dict,
+    predict_verbose
+)
+from transformers import AutoTokenizer
+import torch
+from typing import List, Dict
+
+def visualizer(iteration_log: List[List[Dict]]):
+    # Generate a string to visualize the predictions.
+    strs = ''
+    for sent_id, sent in enumerate(iteration_log):
+        strs += f'=== Line {sent_id} ===\n'
+        for itr_id, itr in enumerate(sent):
+            if itr['tag'] is None:
+                strs += ' '.join(itr['src']).replace('$START', '').strip() + '\n'
+                break
+            strs += f'== Iteration {itr_id} ==\n'
+            src_str = '|'
+            tag_str = '|'
+            for tok, tag in zip(itr['src'], itr['tag']):
+                max_len = max(len(tok), len(tag)) + 1
+                src_str += tok + ' '*(max_len - len(tok)) + '|'
+                tag_str += tag + ' '*(max_len - len(tag)) + '|'
+            strs += src_str + '\n'
+            strs += tag_str + '\n'
+        strs += '\n'
+    return strs
+                
+def main():
+    print("시작 ~ ^^")
+    gector_path = "/home/dashic/gector-test/gector-test/gector"
+
+    model_path = "gotutiyan/gector-roberta-large-5k"
+    input_path = os.path.join(gector_path, "input", "hbcominatya.txt")
+    verb_path = os.path.join(gector_path, "data", "verb-form-vocab.txt")
+    visualize = os.path.join(gector_path, "viz_output", "hbcominatya_rob.txt")
+    out = os.path.join(gector_path, "viz_output", "out.txt")
+    
+    keep_confidence = 0.0
+    min_error_prob = 0.0
+    batch_size = 128
+    n_iteration = 5
+
+    model = GECToR.from_pretrained(model_path).eval()
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    srcs = open(input_path).read().rstrip().split('\n')
+    encode, decode = load_verb_dict(verb_path)
+    if torch.cuda.is_available():
+        model.cuda()
+    predict_args = {
+        'model': model,
+        'tokenizer': tokenizer,
+        'srcs': srcs,
+        'encode': encode,
+        'decode': decode,
+        'keep_confidence': keep_confidence,
+        'min_error_prob': min_error_prob,
+        'batch_size': batch_size,
+        'n_iteration': n_iteration
+    }
+    if visualize is not None:
+        final_corrected_sents, iteration_log = predict_verbose(
+            **predict_args
+        )
+        strs = visualizer(iteration_log)
+        with open(visualize, 'w') as fp:
+            fp.write(strs)
+    else:
+        final_corrected_sents = predict(
+            **predict_args
+        )
+    with open(out, 'w') as f:
+        f.write('\n'.join(final_corrected_sents))
+
+    print(f'=== Finished ===')
+
+if __name__ == '__main__':
+    main()
---
+++ models/grammar_api.py
@@ -0,0 +1,138 @@
+from fastapi import FastAPI, File, UploadFile, Body, HTTPException, Form
+from typing import List
+import torch
+import uvicorn
+import shutil
+from pydantic import BaseModel
+from typing_extensions import Annotated
+import os
+from logging import Logger
+import uvicorn
+
+from typing import Optional, Union, List, Pattern
+from typing_extensions import Literal
+import torch
+import os
+import torch
+import json
+
+from transformers import AutoTokenizer
+
+from gector.gector import (
+    GECToR,
+    load_verb_dict,
+    predict_verbose
+)
+
+from utils.gram_visualizer_json import *
+from utils.gram_out_json import *
+import nltk
+from nltk.tokenize import sent_tokenize
+import json
+
+app = FastAPI()
+# FOAM 관련 참고
+# @app.post("/files/")
+# async def create_file(
+#     fileb: Annotated[UploadFile, File()],
+#     token: Annotated[str, Form()],
+# ):
+#     return {
+#         "token": token,
+#         "fileb_content_type": fileb.content_type,
+#     }
+
+model_path = "gotutiyan/gector-roberta-large-5k"
+#input_path = os.path.join(gector_path, "input", "macominatya.json")
+
+model = GECToR.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model.eval()
+
+@app.post("/upload/")
+async def upload_json(
+    text: Annotated[str, Form()],
+):
+    try:
+        
+        gector_path = "/home/dashic/gector-test/gector-test/gector"
+        verb_path = os.path.join(gector_path, "data", "verb-form-vocab.txt")
+        
+
+        nltk.download('punkt')
+        print(text)
+        srcs = sent_tokenize(text)
+        encode, decode = load_verb_dict(verb_path)
+
+        transcription = text
+        # parameter initialization
+        keep_confidence = 0.0
+        min_error_prob = 0.0
+        batch_size = 128
+        n_iteration = 5
+
+        if torch.cuda.is_available():
+            model.cuda()
+
+        predict_args = {
+            'model': model,
+            'tokenizer': tokenizer,
+            'srcs': srcs,
+            'encode': encode,
+            'decode': decode,
+            'keep_confidence': keep_confidence,
+            'min_error_prob': min_error_prob,
+            'batch_size': batch_size,
+            'n_iteration': n_iteration
+        }
+
+        # Grammar Error Correction Process
+        final_corrected_sents, iteration_log = predict_verbose(
+            **predict_args
+        )
+        checker_data = visualizer_json(iteration_log, final_corrected_sents)
+
+        # dump visualized checker .json file
+        check = os.path.join(gector_path, "check", "final_check_ma.json")
+
+        with open(check, "w", encoding="utf-8") as c:
+            json.dump(checker_data, c, indent="\t")
+
+
+        print(f'=== Visualized Checker JSON Created ===')
+
+
+        # metrics
+        metric_data = {
+            "error_count": int,
+            "main": {}
+        }
+        ctl = get_cleaned_token_list()
+        for og_sent, inner_dict in checker_data.items():
+            if inner_dict["edited"] == True:
+                inner_data = get_scrs_tok(inner_dict, ctl)
+                metric_data["main"][og_sent] = inner_data
+            else:
+                metric_data["main"][og_sent] = {"edited": False, "corrected_sentence": inner_dict["fin_sentence"]}
+
+        error_count = 0
+        for og in metric_data["main"]:
+            v = metric_data["main"][og]
+            if v["edited"] == True:
+                error_count += len(v["action_tag"])
+        metric_data["error_count"] = error_count
+
+        # Run this part if you want the metric json file
+        # metric = os.path.join(gector_path, "metric", "metric.json")
+        # with open(metric, "w", encoding="utf-8") as r:
+        #     json.dump(metric_data, r, indent="\t")
+
+        # Final output
+        phase = "phase_2"
+        out_path = os.path.join(gector_path, "real", f"grammar_{phase}.json")
+        return create_json(phase=phase, out_path=out_path, error_count=error_count, check_data=checker_data)
+    except Exception as e:
+        return {"text": None, "status": f"Error: {str(e)}"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8888)
