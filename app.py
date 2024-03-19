b3f7ed2ffac014f454190a764ee3967feab4d129
C7C4FF <c7c4ff.beom@gmail.com>
2024-03-20 03:06:12 +0900

    fix: app.py 버튼 누를 시 로그인 되도록 변경
    [#19]

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
+login = "http://mopic.site/login"
+
 st.title("MOPIc")
 st.write("구글 아이디로 로그인하시면 자동으로 회원으로 등록됩니다.")
 
-if st.button("Google Login"):
-
-    pass
+st.markdown(
+    f'<a href="{login}" target="_self" style="display: inline-block; padding: 12px 20px; background-color: lightblue; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">GOOGLE LOGIN</a>',
+    unsafe_allow_html=True
+)
