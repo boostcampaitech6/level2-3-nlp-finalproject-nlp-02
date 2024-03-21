97e26e6ade6dab29ea0c6511e8687971ec5edc44
C7C4FF <c7c4ff.beom@gmail.com>
2024-03-21 22:05:49 +0900

    feat: streamlit session 유지용 페이지 생성 및 함수 생성
    session_state으로 유지하기 위해 session.py 페이지를 새로 만들고 진행
    
    [#21]

--- auth_router.py
+++ auth_router.py
@@ -1,14 +1,18 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from pydantic import BaseModel
 
 from database.connection import get_db
 from database.orm import User
 from database.repository import create_user, get_user_by_email
 from oauth import oauth  # main.py 혹은 app의 설정을 import 해야 합니다.
+import requests
 
 router = APIRouter()
 
+class TokenData(BaseModel):
+    token: str
 
 @router.get("/login")
 async def login(request: Request):
@@ -29,7 +33,7 @@ async def auth(request: Request, session: Session = next(get_db())):
 
     request.session["user_info"] = userinfo
 
-    return RedirectResponse(url="/lobby", status_code=303)
+    return RedirectResponse(url="/session", status_code=303)
 
 
 @router.get("/me")
@@ -40,3 +44,17 @@ async def get_current_user(request: Request, session: Session = Depends(get_db))
         return get_user_by_email(session=session, email=user_info["email"])
 
     return {"message": "no session"}
+
+
+
+@router.post("/get-me/")
+async def get_user_info(token_data: TokenData):
+    google_userinfo_url = "https://www.googleapis.com/oauth2/v3/userinfo"
+    headers = {"Authorization": f"Bearer {token_data.token}"}
+    response = requests.get(google_userinfo_url, headers=headers)
+    
+    if response.status_code == 200:
+        user_info = response.json()
+        return user_info
+    else:
+        raise HTTPException(status_code=response.status_code, detail="사용자 정보를 조회할 수 없습니다.")
