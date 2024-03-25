e8653f7afbaa9d1339c0e1a0786401bc65cc3448
minari1505 <minari1505@naver.com>
2024-03-25 10:48:12 +0900

    fix:black으로 코드정리 [#20]

--- pages/test.py
+++ pages/test.py
@@ -7,20 +7,22 @@ import requests
 test = "http://0.0.0.0:0000/test/"
 
 st.title("Daily Test")
-st.image("AVA.png", caption="문제를 두 번 들려드린 후 바로 녹음을 시작해주세요.", width=300)
+st.image(
+    "AVA.png", caption="문제를 두 번 들려드린 후 바로 녹음을 시작해주세요.", width=300
+)
 
 
-#When "listen" button is pressed, Convert .wav->html tag to autoplay
+# When "listen" button is pressed, Convert .wav->html tag to autoplay
 def autoplay_audio(file_path: str):
     with open(file_path, "rb") as audio_file:
         audio_bytes = audio_file.read()
-        audio_base64 = base64.b64encode(audio_bytes).decode('utf-8')
+        audio_base64 = base64.b64encode(audio_bytes).decode("utf-8")
         audio_html = f'<audio autoplay><source src="data:audio/wav;base64,{audio_base64}" type="audio/wav"></audio>'
         st.markdown(audio_html, unsafe_allow_html=True)
 
 
 def save_recording(audio_data):
-    files = {'file': ('recording4.wav', audio_data, 'audio/wav')}
+    files = {"file": ("recording4.wav", audio_data, "audio/wav")}
     response = requests.post(test, files=files)
     print(response.text)
     if response.ok:
@@ -29,14 +31,13 @@ def save_recording(audio_data):
         st.error("Failed to save the recording.")
 
 
-
 def callback():
-    #Check 'my_recorder_output' in st.session_state
-    if 'my_recorder_output' in st.session_state:
-        audio_data = st.session_state.my_recorder_output['bytes']
+    # Check 'my_recorder_output' in st.session_state
+    if "my_recorder_output" in st.session_state:
+        audio_data = st.session_state.my_recorder_output["bytes"]
         if audio_data is not None:
             save_recording(audio_data)
-            st.audio(audio_data, format='audio/wav')
+            st.audio(audio_data, format="audio/wav")
         else:
             st.error("오디오 데이터를 찾을 수 없습니다.")
 
@@ -47,12 +48,13 @@ def save_recording_locally(audio_data):
     href = f'<a href="data:file/wav;base64,{audio_file}" download="recording.wav">Download recording</a>'
     st.markdown(href, unsafe_allow_html=True)
 
-#question file path
-q_audio_path_1 = 'q_1.wav'
-q_audio_path_2 = 'q_2.wav'
-q_audio_path_3 = 'q_3.wav'
 
-#button shape
+# question file path
+q_audio_path_1 = "q_1.wav"
+q_audio_path_2 = "q_2.wav"
+q_audio_path_3 = "q_3.wav"
+
+# button shape
 button_style = """
 <style>
 div.stButton > button:first-child {
@@ -63,10 +65,10 @@ div.stButton > button:first-child {
 """
 st.markdown(button_style, unsafe_allow_html=True)
 
-#Create a holder for the Start Recording button
+# Create a holder for the Start Recording button
 recorder_holder = st.empty()
 
-#Apply a button styles
+# Apply a button styles
 button_style = """
 <style>
 div.stButton > button {
@@ -79,11 +81,11 @@ div.stButton > button {
 st.markdown(button_style, unsafe_allow_html=True)
 
 cols = st.columns([1, 1, 1, 11])
-recorder_holder = st.empty()  #Fix the position of the "Start recording" button
+recorder_holder = st.empty()  # Fix the position of the "Start recording" button
 
 
 def main() -> None:
-    #each question button
+    # each question button
     with cols[0]:
         if st.button("1"):
             autoplay_audio(q_audio_path_1)
@@ -96,9 +98,17 @@ def main() -> None:
         if st.button("3"):
             autoplay_audio(q_audio_path_3)
 
-    #Start & stop recording buttons
+    # Start & stop recording buttons
     with recorder_holder.container():
-        mic_recorder(start_prompt="녹음 시작", stop_prompt="다음", just_once=True, key='my_recorder', use_container_width=True, format="wav", callback=callback)
+        mic_recorder(
+            start_prompt="녹음 시작",
+            stop_prompt="다음",
+            just_once=True,
+            key="my_recorder",
+            use_container_width=True,
+            format="wav",
+            callback=callback,
+        )
 
 
 if __name__ == "__main__":
