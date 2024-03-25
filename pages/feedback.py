db63af255aa3610d2b714dc2adb1ec67c86638cf
C7C4FF <c7c4ff.beom@gmail.com>
2024-03-25 18:37:47 +0900

    feat: 피드백 페이지 레이아웃 생성
    [#39]

--- pages/feedback.py
+++ pages/feedback.py
@@ -1,6 +1,39 @@
 import requests
 import streamlit as st
 
+def fix_sentence(tagged_list):
+    if tagged_list:
+        for tagged in tagged_list:
+            with st.expander(tagged["sentence"]):
+                for i in range(len(tagged["tag"])):
+                    st.write(tagged["ref_word"][i], tagged["tag"][i], tagged["category"][i], tagged["grammar_description"][i])
+    else:
+        return st.write("틀린 문장이 없네요! 아주 잘 하셨어요.")
+
+def make_layout(question_data):
+    by_text, by_speaking = st.tabs(["텍스트", "말하기"])
+
+    with by_text:
+        grammar = question_data["grammar"]["phase_2"]
+        st.subheader("grammar")
+        st.markdown(f"전체 발화 문장 중 올바른 문법 사용 비율은 **{grammar["score"]}%** 입니다.")
+
+        with st.container(height=300):
+            st.markdown(grammar["original_passage"])
+
+        fix_sentence(grammar["tag_grammar_info"])
+
+        st.subheader("coherence")
+        st.markdown(f"- 고객님의 질문에 대한 답변의 주제 적합성은 **{question_data["coherence"]}**입니다")
+        st.subheader("complexity")
+        st.markdown(f"{question_data["complexity"]}")
+
+    with by_speaking:
+        st.markdown(f"전체 발화 중 잘못된 발음 없이 명확하게 발음한 비율은 **{question_data["mpr"]}%** 입니다.")
+        st.markdown(f"연속으로 발화한 평균 단어 수는 **{question_data["mlr"]}개** 입니다.")
+        st.markdown(f"전체 발화 중 pause 비율은 **{question_data["pause"]}%** 입니다.")
+
+
 st.markdown("""
     <style>
         section[data-testid="stSidebar"][aria-expanded="true"]{
@@ -36,20 +69,20 @@ st.markdown(
 )
 
 tab1, tab2, tab3 = st.tabs(["1번", "2번", "3번"])
-
+questions = requests.get(url="http://mopic.today/api/test").json()
 
 with tab1:
     question_response_1 = requests.get(
-        url=f"https://mopic.today/api/me/result/{st.session_state['date']}/1",
+        url=f"http://mopic.today/api/me/result/{st.session_state['date']}/1",
         headers={"access_token": st.session_state["token"]["access_token"]},
     )
-    
+
     if question_response_1.status_code == 200:
         question_data_1 = question_response_1.json()
 
-        st.header(f"{question_data_1['q_num']}")
-        st.write(question_data_1)
-
+        st.write(f"{question_data_1['q_num']}. {questions['q1']}")
+    
+        make_layout(question_data_1)
 
 with tab2:
     question_response_2 = requests.get(
@@ -60,8 +93,8 @@ with tab2:
     if question_response_2.status_code == 200:
         question_data_2 = question_response_2.json()
 
-        st.header(f"{question_data_2['q_num']}")
-        st.write(question_data_2)
+        st.write(f"{question_data_2['q_num']}. {questions['q2']}")
+        make_layout(question_data_2)
 
 with tab3:
     question_response_3 = requests.get(
@@ -72,5 +105,5 @@ with tab3:
     if question_response_3.status_code == 200:
         question_data_3 = question_response_3.json()
 
-        st.header(f"{question_data_3['q_num']}")
-        st.write(question_data_3)
+        st.write(f"{question_data_3['q_num']}. {questions['q3']}")
+        make_layout(question_data_3)
