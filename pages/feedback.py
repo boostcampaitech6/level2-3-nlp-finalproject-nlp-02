47e43c485e401e076327e709869ec95a936b1127
C7C4FF <c7c4ff.beom@gmail.com>
2024-03-25 15:22:18 +0900

    fix: feedback 페이지 변수명 변경
    [#39]

--- pages/feedback.py
+++ pages/feedback.py
@@ -11,13 +11,13 @@ st.markdown("""
 
 
 response = requests.get(
-    url=f"https://mopic.today/api/me/result/{st.session_state['date']}",
+    url=f"http://mopic.today/api/me/result/{st.session_state['date']}",
     headers={"access_token": st.session_state["token"]["access_token"]},
 )
 if response.status_code == 200:
     data = response.json()
 
-st.title(f"{data['createdDate']} 시험 결과")
+st.title(f"{data['date']} 시험 결과")
 
 st.markdown(
     """
@@ -31,46 +31,46 @@ st.markdown(
 )
 
 st.markdown(
-    f"당신의 예상 등급은 <span class='score'>{data['finalscore']}</span> 입니다.",
+    f"당신의 예상 등급은 <span class='score'>{data['score']}</span> 입니다.",
     unsafe_allow_html=True,
 )
 
 tab1, tab2, tab3 = st.tabs(["1번", "2번", "3번"])
-# TODO: 해당 시험의 날짜를 받아오도록 함수 변경
+
 
 with tab1:
-    question_response = requests.get(
-        url=f"https://mopic.today/api/me/result/{st.session_stae['date']/1}",
+    question_response_1 = requests.get(
+        url=f"https://mopic.today/api/me/result/{st.session_state['date']}/1",
         headers={"access_token": st.session_state["token"]["access_token"]},
     )
     
-    if question_response.status_code == 200:
-        question_data = question_response.json()
+    if question_response_1.status_code == 200:
+        question_data_1 = question_response_1.json()
 
-        st.header(f"{question_data['q_num']}")
-        st.write(question_data)
+        st.header(f"{question_data_1['q_num']}")
+        st.write(question_data_1)
 
 
 with tab2:
-    question_response = requests.get(
-        url=f"https://mopic.today/api/me/result/{st.session_stae['date']/2}",
+    question_response_2 = requests.get(
+        url=f"http://mopic.today/api/me/result/{st.session_state['date']}/2",
         headers={"access_token": st.session_state["token"]["access_token"]},
     )
     
-    if question_response.status_code == 200:
-        question_data = question_response.json()
+    if question_response_2.status_code == 200:
+        question_data_2 = question_response_2.json()
 
-        st.header(f"{question_data['q_num']}")
-        st.write(question_data)
+        st.header(f"{question_data_2['q_num']}")
+        st.write(question_data_2)
 
 with tab3:
-    question_response = requests.get(
-        url=f"https://mopic.today/api/me/result/{st.session_stae['date']/3}",
+    question_response_3 = requests.get(
+        url=f"http://mopic.today/api/me/result/{st.session_state['date']}/3",
         headers={"access_token": st.session_state["token"]["access_token"]},
     )
     
-    if question_response.status_code == 200:
-        question_data = question_response.json()
+    if question_response_3.status_code == 200:
+        question_data_3 = question_response_3.json()
 
-        st.header(f"{question_data['q_num']}")
-        st.write(question_data)
+        st.header(f"{question_data_3['q_num']}")
+        st.write(question_data_3)
